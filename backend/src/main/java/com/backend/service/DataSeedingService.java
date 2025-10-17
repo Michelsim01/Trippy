c470475e75ab82dd523c29928a617269a9e1a81d
euuren <e1156543@u.nus.edu>
2025-10-17 22:32:02 +0800

    fix backend application test

--- backend/src/main/java/com/backend/service/DataSeedingService.java
+++ backend/src/main/java/com/backend/service/DataSeedingService.java
@@ -3,6 +3,7 @@ package com.backend.service;
 import com.backend.entity.*;
 import com.backend.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -16,6 +17,7 @@ import java.util.Random;
 import java.util.UUID;
 
 @Service
+@Profile("!test")
 public class DataSeedingService {
 
     @Autowired
